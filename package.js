9d12b926884eafe89b1a909d7688d42beb162d69
Patrick Moore <patmoore@farreach.es>
2014-09-04 14:36:24 -0700

    update package information

--- package.js
+++ package.js
@@ -1,19 +1,21 @@
 Package.describe({
-  summary: "Simple file uploading for Meteor"
+  summary: "Simple file uploading for Meteor",
+  version:"0.9",
+  git:"https://github.com/whalepath/meteor-file.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use(["underscore", "ejson"], ["client", "server"]);
   api.use(["templating", "spacebars", "ui"], "client");
-  api.add_files(["meteor-file.js"], ["client", "server"]);
-  api.add_files(["meteor-file-uploader.html", "meteor-file-uploader.js"], "client");
+  api.addFiles(["meteor-file.js"], ["client", "server"]);
+  api.addFiles(["meteor-file-uploader.html", "meteor-file-uploader.js"], "client");
 
   if (typeof api.export !== 'undefined') {
     api.export("MeteorFile", ["client", "server"]);
   }
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(["meteor-file", "tinytest", "test-helpers"]);
   api.add_files("meteor-file-test.js", ["client", "server"]);
 });
